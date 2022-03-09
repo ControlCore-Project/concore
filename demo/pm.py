18cc08d3d441dcaeae0b6d1cfea38bcc00df0162
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2022-03-08 23:38:30 -0500

    Refactor and clean the comments

--- demo/pm.py
+++ demo/pm.py
@@ -1,18 +1,15 @@
 import concore
 import numpy as np
 
-#//pm function//
+# pm function
 def pm(u):
   return u + 0.01
 
-#//main//
-
-concore.default_maxtime(150) ##maps to-- for i in range(0,150):
+# main
+concore.default_maxtime(150)
 concore.delay = 0.02
 
-#//initial values-- transforms to string including the simtime as the 0th entry in the list//
-# u = np.array([[0.0]])
-# ym = np.array([[0.0]]) 
+# initial values-- transforms to string including the simtime as the 0th entry in the list
 init_simtime_u = "[0.0, 0.0]"
 init_simtime_ym = "[0.0, 0.0]"
 
@@ -24,13 +21,7 @@ while(concore.simtime<concore.maxtime):
     #####
     ym = pm(u)
     #####
-    print(str(concore.simtime) + ". u="+str(u) + "ym="+str(ym));
+    print(str(concore.simtime) + ". u="+str(u) + "ym="+str(ym))
     concore.write(1,"ym",list(ym.T[0]),delta=1)
 
 print("retry="+str(concore.retrycount))
-
-
-#// main-- to begin with//
-#for i in range(0,150):
-#  ym = pm(u)
-#  print('u='+repr(u)+' ym='+repr(ym))
