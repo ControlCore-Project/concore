21b3c62a3f4a63255243395744843ad7d2525fb3
markgarnold <78613198+markgarnold@users.noreply.github.com>
2022-09-07 14:04:45 -0400

    hcm_bang to play with

---
+++ tools/bangbang.py
@@ -0,0 +1,30 @@
+import numpy as np
+import concore
+
+
+def bangbang_controller(ym):
+    amp = 0
+    if ym[0]>70:
+        amp = 3
+    elif ym[0]<65:
+        amp = 1
+	    
+     
+    ustar = np.array([amp,30])    
+    return ustar
+
+
+concore.default_maxtime(150)
+concore.delay = 0.02
+init_simtime_u = "[0.0, 0.0,0.0]"
+init_simtime_ym = "[0.0, 70.0,91]"
+u = np.array([concore.initval(init_simtime_u)]).T
+while(concore.simtime<concore.maxtime):
+    while concore.unchanged():
+        ym = concore.read(1,"ym",init_simtime_ym)
+    ym = np.array(ym)
+    
+    ustar = bangbang_controller(ym)
+    
+    print(str(concore.simtime) + " u="+str(ustar) + "ym="+str(ym))
+    concore.write(1,"u",list(ustar),delta=0)
