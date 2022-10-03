e7e9f20f6e0abb0f94148a8ffeabed5993c70442
markgarnold <78613198+markgarnold@users.noreply.github.com>
2022-10-03 14:40:44 -0400

    remove globals from mayuresh

--- tools/pidmayuresh2.py
+++ tools/pidmayuresh2.py
@@ -2,6 +2,9 @@ import numpy as np
 import math
 import concore
 dT = 0.1
+global freq
+Prev_Error = 0
+I = 0
 
 sp = concore.tryparam('sp', 67.5)
 Kp = concore.tryparam('Kp', 0.075)
@@ -11,9 +14,8 @@ freq = concore.tryparam('freq',30)
 sigout = concore.tryparam('sigout',True)
 cin = concore.tryparam('cin', 'hr')
 
-def  pid_controller(state, ym, sp, Kp, Ki, Kd, sigout, cin, low, up):
-    Prev_Error = state[0]
-    I = state[1]
+def  pid_controller(Prev_Error, I, ym):
+    global freq
     if cin == 'hr':
         Error = sp - ym[1]
     elif cin == 'map':
@@ -27,26 +29,24 @@ def  pid_controller(state, ym, sp, Kp, Ki, Kd, sigout, cin, low, up):
     amp = Kp*P + Ki*I + Kd*D
     Prev_Error = Error      
     if sigout:
-        amp = (up-low)/(1.0 + math.exp(amp)) + low
-    state = [Prev_Error, I]
-    return (state, amp)
+        amp = 3.0/(1.0 + math.exp(amp))
+    ustar = np.array([amp,freq])    
+    return (Prev_Error, I, ustar)
 
 
 concore.default_maxtime(150)
 concore.delay = 0.02
 init_simtime_ym = "[0.0, 70.0,91]"
 ym = np.array(concore.initval(init_simtime_ym))
-state = [0.0, 0.0]
 print("Mayuresh's PID controller: sp is "+str(sp))
 print(concore.params)
 while(concore.simtime<concore.maxtime):
     while concore.unchanged():
         ym = concore.read(1,"ym",init_simtime_ym)
     ym = np.array(ym)
-    (state,amp) =  pid_controller(state,ym,sp,Kp,Ki,Kd,sigout,cin,0,3)
-    u = np.array([amp,freq])    
-    print(str(concore.simtime) + " u="+str(u) + "ym="+str(ym))
-    concore.write(1,"u",list(u),delta=0)
+    (Prev_Error, I, ustar) =  pid_controller(Prev_Error, I, ym)
+    print(str(concore.simtime) + " u="+str(ustar) + "ym="+str(ym))
+    concore.write(1,"u",list(ustar),delta=0)
 
 
 
