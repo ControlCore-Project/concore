88f0f5c468606aeb0850e191068d84b8d31669cb
markgarnold <78613198+markgarnold@users.noreply.github.com>
2023-04-02 18:07:31 -0400

    pacq overlap extract

--- nintan/pacq0.py
+++ nintan/pacq0.py
@@ -2,34 +2,34 @@ import concore
 import time
 import threading
 
-global ymglobal,counter
-ymglobal = 0
+TIME_SAMPLE = 5
+global counter
 counter = 0
+
 
 def extract(ampD):
     s = 0.0
     for data in ampD:
        s += data
+       time.sleep(TIME_DATA/4) #make extract really slow for testing
     return s/len(ampD)
 
 def acq():
-    global ymglobal,counter
-    ampT = []
-    ampD = []
+    global counter
+    global ampT,ampD
     for block in range(numBlocks):
         for frame in range(framesPerBlock):
             ampT.append(counter + block*framesPerBlock+frame)
             ampD.append(counter + abs(frame-framesPerBlock/2))
+            time.sleep(TIME_DATA)
     print(ampT)
     print(ampD)
-    ymglobal = extract(ampD)
-    print("ymglobal="+str(ymglobal))
     counter = counter +  numBlocks*framesPerBlock 
-    time.sleep(5)
 
 #initialization
 numBlocks = 3
 framesPerBlock = 5
+TIME_DATA = TIME_SAMPLE /(numBlocks*framesPerBlock)
         
 concore.delay = 0.01
 init_simtime_u = "[0.0, 0.0, 0.0]"
@@ -39,16 +39,20 @@ ym = concore.initval(init_simtime_ym)
 
 acq_thread = threading.Thread(target=acq, daemon=True)
 nxtacq_thread = threading.Thread(target=acq, daemon=True)
+ampT = []
+ampD = []
 acq_thread.start()
 while(concore.simtime<concore.maxtime):
     while concore.unchanged():
         ym = concore.read(1,"ym",init_simtime_ym)
     acq_thread.join()
-    ym[0]  = ymglobal 
     acq_thread = nxtacq_thread
+    oldAmpD = ampD
+    ampT = []
+    ampD = []
     acq_thread.start()
     nxtacq_thread = threading.Thread(target=acq, daemon=True)
-    print("")
+    ym[0] = extract(oldAmpD)
     print("ym="+str(ym[0]));
     concore.write(1,"ym",ym)
 acq_thread.join()
--- nintan/pacq0a.py
+++ nintan/pacq0a.py
@@ -2,34 +2,34 @@ import concore
 import time
 import threading
 
-TIME_SAMPLE = 5
-global counter
+global ymglobal,counter
+ymglobal = 0
 counter = 0
-
 
 def extract(ampD):
     s = 0.0
     for data in ampD:
        s += data
-       time.sleep(TIME_DATA/4) #make extract really slow for testing
     return s/len(ampD)
 
 def acq():
-    global counter
-    global ampT,ampD
+    global ymglobal,counter
+    ampT = []
+    ampD = []
     for block in range(numBlocks):
         for frame in range(framesPerBlock):
             ampT.append(counter + block*framesPerBlock+frame)
             ampD.append(counter + abs(frame-framesPerBlock/2))
-            time.sleep(TIME_DATA)
     print(ampT)
     print(ampD)
+    ymglobal = extract(ampD)
+    print("ymglobal="+str(ymglobal))
     counter = counter +  numBlocks*framesPerBlock 
+    time.sleep(5)
 
 #initialization
 numBlocks = 3
 framesPerBlock = 5
-TIME_DATA = TIME_SAMPLE /(numBlocks*framesPerBlock)
         
 concore.delay = 0.01
 init_simtime_u = "[0.0, 0.0, 0.0]"
@@ -39,20 +39,16 @@ ym = concore.initval(init_simtime_ym)
 
 acq_thread = threading.Thread(target=acq, daemon=True)
 nxtacq_thread = threading.Thread(target=acq, daemon=True)
-ampT = []
-ampD = []
 acq_thread.start()
 while(concore.simtime<concore.maxtime):
     while concore.unchanged():
         ym = concore.read(1,"ym",init_simtime_ym)
     acq_thread.join()
+    ym[0]  = ymglobal 
     acq_thread = nxtacq_thread
-    oldAmpD = ampD
-    ampT = []
-    ampD = []
     acq_thread.start()
     nxtacq_thread = threading.Thread(target=acq, daemon=True)
-    ym[0] = extract(oldAmpD)
+    print("")
     print("ym="+str(ym[0]));
     concore.write(1,"ym",ym)
 acq_thread.join()
