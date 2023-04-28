75b9ab7f3425f07735e32172466060e7e7a23c00
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2023-04-28 15:30:40 -0400

    Update cstim.py

--- nintan/cstim.py
+++ nintan/cstim.py
@@ -6,7 +6,12 @@ try:
     bangbang = concore.params['bang']
 except:
     bangbang = False
-
+    
+try:
+    freq_control = concore.params['freq_control']
+except:
+    freq_control = False
+    
 concore.delay = 0.01
 init_simtime_u = "[0.0, 0.0, 0.0]"
 init_simtime_ym = "[0.0, 0.0, 0.0]"
@@ -18,7 +23,15 @@ wallclock1 = time.perf_counter()
 while(concore.simtime<concore.maxtime):
     while concore.unchanged():
         ym = concore.read(1,"ym",init_simtime_ym)
-    if bangbang:
+        
+    if freq_control:
+        u[0] = abs(ym[1])/100.0
+        if abs(ym[1]) > 100:
+            u[0] = 1.0
+        elif abs(ym[1]) < 1:
+            u[0] = 0.01
+            
+    elif bangbang:
         if ym[0] > 0:
             u[0] = 0.9
         else:
