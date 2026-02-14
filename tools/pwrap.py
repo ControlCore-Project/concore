8dead92c922c72c2f94ad89d1d0584035310d7de
Avinash Kumar Deepak <avinash8655279@gmail.com>
2026-02-14 13:34:15 +0530

    Security: Use HTTPS for API requests - fixes #309

--- tools/pwrap.py
+++ tools/pwrap.py
@@ -11,43 +11,43 @@ concore.delay = 0.02
 
 try:
     apikey=open(concore.inpath+'1/concore.apikey',newline=None).readline().rstrip()
-except:
+except (OSError, IOError):
     try:
         #perhaps this should be removed for security
         apikey=open('./concore.apikey',newline=None).readline().rstrip()
-    except:
+    except (OSError, IOError):
         apikey = ''
 
 try:
     yuyu=open(concore.inpath+'1/concore.yuyu',newline=None).readline().rstrip()
-except:
+except (OSError, IOError):
     try:
         yuyu=open('./concore.yuyu',newline=None).readline().rstrip()
-    except:
+    except (OSError, IOError):
         yuyu = 'yuyu'
 
 try:
     name1=open(concore.inpath+'1/concore.name1',newline=None).readline().rstrip()
-except:
+except (OSError, IOError):
     try:
         name1=open('./concore.name1',newline=None).readline().rstrip()
-    except:
+    except (OSError, IOError):
         name1 = 'u'
 
 try:
     name2=open(concore.inpath+'1/concore.name2',newline=None).readline().rstrip()
-except:
+except (OSError, IOError):
     try:
         name2=open('./concore.name2',newline=None).readline().rstrip()
-    except:
+    except (OSError, IOError):
         name2 = 'ym'
 
 try:
     init_simtime_u = open(concore.inpath+'1/concore.init1',newline=None).readline().rstrip()
-except:
+except (OSError, IOError):
     try:
         init_simtime_u = open('./concore.init1',newline=None).readline().rstrip()
-    except:
+    except (OSError, IOError):
         init_simtime_u = "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
 
 with open("./"+name1,"w") as fcopy:
@@ -55,10 +55,10 @@ with open("./"+name1,"w") as fcopy:
 
 try:
     init_simtime_ym = open(concore.inpath+'1/concore.init2',newline=None).readline().rstrip()
-except:
+except (OSError, IOError):
     try:
         init_simtime_ym = open('./concore.init2',newline=None).readline().rstrip()
-    except:
+    except (OSError, IOError):
         init_simtime_ym = "[0.0, 0.0, 0.0]"
 
 print(apikey)
@@ -98,7 +98,7 @@ while(concore.simtime<concore.maxtime):
     if len(r.text)!=0:
         try:
             t=literal_eval(r.text)[0]
-        except:
+        except Exception:
             print("bad eval "+r.text)
     timeout_count = 0
     t1 = time.perf_counter()
@@ -110,7 +110,7 @@ while(concore.simtime<concore.maxtime):
         f = {'file1': open(concore.inpath+'1/'+name2, 'rb')}
         try:
             r = requests.post('https://www.controlcore.org/ctl/'+yuyu+apikey+'&fetch='+name1, files=f,timeout=timeout_max)
-        except:
+        except Exception:
             print("PW: bad requests")
         timeout_count += 1
         if r.status_code!=200 or time.perf_counter()-t1 > 1.1*timeout_max: #timeout_count>200:
@@ -119,7 +119,7 @@ while(concore.simtime<concore.maxtime):
         if len(r.text)!=0:
             try:
                 t=literal_eval(r.text)[0]
-            except:
+            except Exception:
                 print("bad eval "+r.text)
     oldt = t
     oldu = r.text
