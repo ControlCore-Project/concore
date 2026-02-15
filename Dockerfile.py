bb996f12035998a45036cc6e47984f763fa7eeb9
GREENRAT-K405 <paramkanada2024@gmail.com>
2026-02-15 19:16:40 +0530

    optimize python dockerfile to be much lighter

--- Dockerfile.py
+++ Dockerfile.py
@@ -1,9 +1,8 @@
-FROM jupyter/base-notebook
+FROM python:3.10-slim
  
-USER root
-RUN apt-get update && apt-get install -y build-essential g++ libgl1-mesa-glx libx11-6
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-COPY . /src 
 WORKDIR /src
+RUN apt-get update && apt-get install -y build-essential g++ libgl1-mesa-glx libx11-6 && rm -rf /var/lib/apt/lists/*
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
 
